4de8f171d9c23468fd35abfeb49e0c967555f02d
angelo.guerrero <angelo.guerrero@golabstech.com>
2020-06-05 20:13:15 -0600

    first commit

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-dataset = pd.read_csv("dataset.csv")
+dataset = pd.read_csv("winequality-red.csv", sep = ';')
 
 x= dataset.iloc[:, [0,1] ].values
 
